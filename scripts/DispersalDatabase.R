498972da2d0090285ccf5eb90a0b22ead3b6d085
Wilkinson <xr49abiw@idiv.de>
2023-08-21 10:01:28 +0200

    Editing r code to clean the dispersal database for analysis

--- scripts/DispersalDatabase.R
+++ scripts/DispersalDatabase.R
@@ -1,8 +1,100 @@
 library(tidyverse)
+library(dplyr)
 
 setwd('C:/Users/xr49abiw/Documents/DispersalProject/data')
 
 db <- read.csv("DispersalUntransformed.csv")
+
+
+####Figuring out data gaps#### 
+count(db, is.na(Body.mass)) # Na = 1216 dp
+
+dbna <- filter(db, is.na(Body.mass))
+
+dbnasp<- count(dbna, Taxon, Species_ID_gbif)#Amphibian (149), bird (2), fish (881), invertebrate (116), mammal(68)
+
+dbna |> 
+  pull(Species_ID_gbif) |>
+  n_distinct() 
+#268 species 
+
+# Need to find the body masses for as many of these species as we can, FishBase, avibase, amphibia web, invertebrate trait database
+
+
+####Tidying up the data####
+#Remove na
+db1 <- db |>
+  filter(!is.na(Body.mass))
+
+#converting units to meters
+# Conversion factors
+conversion_factors <- data.frame(
+  unit = c("ha", "km", "miles"),
+  factor = c(10000, 1000, 1609.34)
+)
+
+db2 <- db1 |>
+  left_join(conversion_factors, by = c("Units" = "unit")) |>
+  mutate(
+    Value = case_when(
+      !is.na(Value) & Units == "km/h" ~ Value * (1000 / 3600), # Convert km/h to m/h
+      !is.na(Value) & Units %in% c("ha", "km", "miles") ~ Value * factor, # Convert ha, km, miles to m
+      TRUE ~ Value
+    ),
+    Unit = case_when(
+      !is.na(Value) & Units == "km/h" ~ "m/h", # Change unit for km/h to m/h
+      !is.na(Value) & Units %in% c("ha", "km", "miles") ~ "m", # Change unit for ha, km, miles to m
+      TRUE ~ Units
+    )
+  ) |>
+  relocate(Unit, .after = `Value`) |>
+  select(-Units, -factor)
+
+
+#converting trait database body mass values
+# Conversion factors
+conversion_factors1 <- data.frame(
+  unit = c("kg", "log(10)g"),
+  factor = c(1000, 1) # Conversion to grams
+)
+
+db3 <- db2 |>
+  left_join(conversion_factors1, by = c("Body.mass.database.Unit" = "unit")) |>
+  mutate(
+    Body.mass = case_when(
+      Body.mass.database.Unit %in% c("kg", "log(10)g") & !is.na(Body.mass) ~ Body.mass * factor,
+      TRUE ~ Body.mass
+    ),
+    Body.mass.db.Unit.convert = case_when(
+      Body.mass.database.Unit %in% c("kg", "log(10)g") ~ "g",
+      TRUE ~ Body.mass.database.Unit
+    )
+  ) |>
+  relocate(Body.mass.db.Unit.convert, .after = Body.mass.Units) |>
+  select(-Body.mass.database.Unit, -factor)
+
+
+#Converting movement database body mass values
+# Conversion factor for kg to g
+conversion_factor_kg_to_g <- 1000
+
+db4 <- db3 |>
+  mutate(
+    Body.mass = case_when(
+      Body.mass.Units == "kg" & !is.na(Body.mass) ~ Body.mass * conversion_factor_kg_to_g,
+      TRUE ~ Body.mass
+    ),
+    Body.mass.Units.convert = case_when(
+      Body.mass.Units == "kg" ~ "g",
+      TRUE ~ Body.mass.Units
+    )
+  ) |>
+  relocate(Body.mass.Units.convert, .after = Body.mass.Units) |>
+  select(-Body.mass.Units)
+
+
+
+
 
 
 
